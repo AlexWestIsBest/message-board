05142078c3975e0def4d5e2151f06923ec135604
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-01-03 14:00:18 -0500

    Builds seed data

---
+++ seedData.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        name: `Alex Westerlund`,
+        text: `This is the first post on my new message Board! Thanks for visiting here.`,
+        image: ``,
+    },
+    {
+        name: `CoffeeLover2201`,
+        text: ``,
+        image: `https://i.imgur.com/1qWUj62.png`, // Midjourney coffee art
+    },
+    {
+        name: `Aristotle`,
+        text: `The most important relationship we can all have is the one you have with yourself, the most important journey you can take is one of self-discovery. To know yourself, you must spend time with yourself, you must not be afraid to be alone. Knowing yourself is the beginning of all wisdom.`,
+        image: ``,
+    },
+    {
+        name: `Ohio Tourism Board`,
+        text: `Come visit Ash Cave, the largest overhang cave east of the Mississippi River!`,
+        image: `https://i.imgur.com/eQzCnC8.jpg`, // Photo of Ash Cave
+    },
+    {
+        name: `Eleanor the Cat`,
+        text: ``,
+        image: `https://i.imgur.com/Mf5GFDV.jpg`, // Photo of Eleanor
+    },
+]
