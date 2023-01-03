ac1d54f479f82def9e74774098f259e2dc0f80da
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-01-03 12:06:43 -0500

    Creates blank newsfeed Router

--- controllers/newsfeed.js
+++ controllers/newsfeed.js
@@ -1,73 +1,36 @@
+// ===DEPENDENCIES===
 const express = require(`express`)
 const newsfeed = express.Router()
-const Product = require(`../models/productSchema.js`)
+const Message = require(`../models/productSchema.js`)
 const app = express();
 app.use(express.static(`.`))
 
+
+
 // ===SEED ROUTE===
-const seedData = require(`../seedData.js`) // Seed Data
-productRouter.get(`/seed`, (req, res) => {
-    Product.deleteMany({}, (error, allProducts) => {})
-    Product.create(seedData, (err, data) => {
-        res.redirect(`/products`)
-    })
-})
+
 
 
 // ===ROUTES===
-// INDEX
-productRouter.get(`/`, (req, res) => {
-    Product.find({}, (err, products) => {
-        res.render(`index.ejs`, {products, tabTitle: `Index Page`})
-    })
-})
+// Index
 
-// NEW
-productRouter.get(`/new`, (req, res) => {
-    res.render(`new.ejs`, {tabTitle: `New Product Page`})
-})
 
-// DELETE
-productRouter.delete(`/:id`, (req, res) => {
-    Product.findByIdAndDelete(req.params.id, (err, data) => {
-        res.redirect(`/products`)
-    })
-})
+// New
 
-// UPDATE
-productRouter.put(`/:id`, (req, res) => {
-    Product.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new: true},
-        (err, updatedProduct) => {
-            res.redirect(`/products/${req.params.id}`)
-        },
-    )
-})
 
-// CREATE
-productRouter.post(`/`, (req, res) => {
-    console.log(`Test`)
-    Product.create(req.body, (err, createdProduct) => {
-        // res.send(createdProduct)
-        res.redirect(`/products`)
-    })
-})
+// Delete
 
-// EDIT
-productRouter.get(`/:id/edit`, (req, res) => {
-    Product.findById(req.params.id, (error, foundProduct) => {
-        res.render(`edit.ejs`, {p : foundProduct, tabTitle: `Edit Page`})
-    })
-})
 
-// SHOW
-productRouter.get(`/:id`, (req, res) => {
-    Product.findById(req.params.id, (err, foundProduct) => {
-        // res.send(foundProduct)
-        res.render(`show.ejs`, {p : foundProduct, tabTitle: `Show Page`})
-    })
-})
+// Update
+
+
+// Create
+
+
+// Edit
+
+
+// Show
+
 
 module.exports = productRouter
