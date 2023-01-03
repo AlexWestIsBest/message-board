7d5ceb85a7a17805709e629516b7b406f28da9a3
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-01-03 10:55:10 -0500

    Cleans up server code

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Dependencies
+// ===DEPENDENCIES===
 const express = require(`express`);
 const mongoose = require(`mongoose`);
 const Message = require(`./models/message.js`)
@@ -7,30 +7,40 @@ const methodOverride = require(`method-override`)
 const newsfeed = express.Router()
 require(`dotenv`).config()
 
+
+
+// ===MIDDLEWARE===
 const app = express();
 app.use(methodOverride(`_method`))
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(`.`))
 
-// Database Configuration
+
+
+// ===DATABASE===
+// Configuration
 const PORT = process.env.PORT;
 const DATABASE_URI = process.env.DATABASE_URI;
 const db = mongoose.connection;
 
-// Database Connection
+// Connection
 mongoose.connect(DATABASE_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
 
-// Database Connection Error/Success
+// Connection Error/Success
 db.on('error', (err) => console.log(err.message + ' is MongoDB not running?'));
 db.on('connected', () => console.log('[Server startup] Mongo connected'));
 db.on('disconnected', () => console.log('[Server error] Mongo disconnected'));
 
-// ROUTES / CONTROLLERS
+
+
+// ===ROUTES===
 // const newsfeedController = require(`./controllers/newsfeed.js`)
 // app.use(`/newsfeed`, newsfeedController)
 
-// App Listener
+
+
+// ===LISTENER===
 app.listen(PORT, () => console.log(`[Server startup] Express is listening on port: ${PORT}`));
